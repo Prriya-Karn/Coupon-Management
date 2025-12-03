0da9a8984362edfae84bad26c64f2ee2b4af8b37
Priya Karn <priya.karn.rn51@gmail.com>
2025-12-03 09:48:26 +0530

    updated server.js

--- server.js
+++ server.js
@@ -3,14 +3,21 @@ const express = require("express");
 const app = express();
 app.use(express.json());
 
+// Home route
+app.get("/", (req, res) => {
+    res.send("Coupon API Running Successfully 🚀");
+});
+
 // routers
 const couponRouter = require("./router/couponRouter");
 const authRouter = require("./router/authRouter");
 
+// All API routes
 app.use("/api", couponRouter);
 app.use("/api", authRouter);
 
 const PORT = process.env.PORT || 3000;
-app.listen(3000, () => {
-    console.log("Server running on port 3000");
+
+app.listen(PORT, () => {
+    console.log("Server running on port " + PORT);
 });
